5ad99de11883fc5da8e203cd438ab7fea7be183c
allaiar <radzhapov.allayar@gmail.com>
2023-02-24 21:51:49 +0600

    new section

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".burger");
+const links = document.querySelector(".header__links");
+
+burger.addEventListener("click", function () {
+  burger.classList.toggle("burger__active");
+  links.classList.toggle("header__links_active");
+});
